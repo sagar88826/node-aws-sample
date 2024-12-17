401c64ac8b285a2bf79a0656784e25fdeb67e3b0
sagar88826 <sagarmandal88826@gmail.com>
2024-12-17 19:34:20 +0530

    test 18

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ const requestListener = (req, res) => {
 };
 const server = http.createServer(requestListener);
 server.listen(8000);
-console.log(process.env.SAGAR);
+console.log(process.env);
 console.log("Application running on Port 8000");
